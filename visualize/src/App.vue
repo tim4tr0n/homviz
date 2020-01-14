fb1f8298ae42d71fd22551b8290b8c1faeaa727a
dukeeagle <luke.igel@gmail.com>
2020-01-13 19:30:27 -0800

    refactor for homuncular purposes

--- visualize/src/App.vue
+++ visualize/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Homunculus msg="Welcome to Your Vue.js App" id="homunculus"/>
+    <p>Bottom Text</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homunculus from './components/Homunculus.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Homunculus
   }
 }
 </script>
@@ -23,6 +23,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 2px;
+}
+#homunculus {
+  height: 90%;
+  vertical-align: top;
 }
 </style>
--- visualize/src/components/Homunculus.vue
+++ visualize/src/components/Homunculus.vue
@@ -15,7 +15,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Homunculus',
   props: {
     msg: String
   },
