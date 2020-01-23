35f1b8040d0b29dbed8ed7c1df27ef504106c55f
dukeeagle <luke.igel@gmail.com>
2020-01-22 17:39:54 -0800

    fix api key exposure

---
+++ visualize/src/credentials.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXX", // YOUR API KEY HERE
+    authDomain: "literary-homunculus.firebaseapp.com",
+    databaseURL: "https://literary-homunculus.firebaseio.com",
+    projectId: "literary-homunculus",
+    storageBucket: "literary-homunculus.appspot.com",
+    messagingSenderId: "243950825999",
+    appId: "XXXXXXXXXX", // YOUR APP ID HERE
+    measurementId: "G-CKCPFJNTGZ"
+  };
+
+  export { firebaseConfig }
