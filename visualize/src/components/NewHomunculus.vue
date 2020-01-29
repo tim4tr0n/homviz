9bb8c9e0192fdad20d1e0a24c191c3b7a0fae8bd
dukeeagle <luke.igel@gmail.com>
2020-01-28 20:17:05 -0800

    temp so i can pull the latest shit

--- visualize/src/components/NewHomunculus.vue
+++ visualize/src/components/NewHomunculus.vue
@@ -5,7 +5,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-// import * as dat from 'dat.gui';
+import * as dat from 'dat.gui';
 // import { Character } from "./../3d/Character"
 // import testconfig from "./../../public/models/skinned/testconfig.json"
 // import axios from 'axios';
@@ -31,6 +31,8 @@ export default {
             2000
         )
         
+        this.morphCtrls = [];
+        this.morphFolder = null;
         this.renderer = new THREE.WebGLRenderer()
         this.renderer.setSize(window.innerWidth, window.innerHeight)
         document.body.appendChild(this.renderer.domElement)
@@ -39,8 +41,9 @@ export default {
 
         const texture = new THREE.TextureLoader().load( require("./../3d/airman.jpg") );
         const geometry = new THREE.BoxGeometry(10, 10, 10)
-        const material = new THREE.MeshBasicMaterial({ map: texture})
+        const material = new THREE.MeshBasicMaterial({ map: texture, wireframe: true })
         
+
         this.light = new THREE.DirectionalLight( 0xffffff, 1 );
         this.light.position.set( 0, -1, 0 );
         this.light.position.set( 13, 5, 0 );
@@ -50,6 +53,7 @@ export default {
         this.cube = new THREE.Mesh(geometry, material)
         
         this.loader = new GLTFLoader();
+        this.   loader.setCrossOrigin('anonymous');
         // this.loader.load(
         //     // resource URL
         //     'Parrot.glb',
@@ -65,12 +69,12 @@ export default {
         //         gltf.asset; // Object
 
         //     },
-        //     // called while loading is progressing
-        //     function ( xhr ) {
+            // called while loading is progressing
+            // function ( xhr ) {
 
-        //         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+            //     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-        //     },
+            // },
         //     // called when loading has errors
         //     function ( error ) {
                 
@@ -78,26 +82,28 @@ export default {
         //         console.error(error)
         //     }
         // );
-        this.loader.load('http://localhost:8081/testhom.glb', (gltf) => {
-            const root = gltf.scene;
-            console.log(gltf)
-            this.scene.add(root);
+        this.loader.load(
+            'http://localhost:8081/guy.gltf',
+            (gltf) => {
+                const root = gltf.scene;
+                console.log(gltf)
+                gltf.scene.scale.set(10,10,10)
+                this.scene.add(root);
 
-            // // compute the box that contains all the stuff
-            // // from root and below
-            // const box = new THREE.Box3().setFromObject(root);
+                root.position.set(0,0,0)
+            },
+            // called while loading is progressing
+            ( xhr ) => {
+                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-            // const boxSize = box.getSize(new THREE.Vector3()).length();
-            // const boxCenter = box.getCenter(new THREE.Vector3());
-
-            // // set the camera to frame the box
-            // frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
-
-            // // update the Trackball controls to handle the new size
-            // controls.maxDistance = boxSize * 10;
-            // controls.target.copy(boxCenter);
-            // controls.update();
-        });
+            },
+            // called when loading has errors
+            ( error ) => {
+                console.log( 'An error happened' );
+                console.error(error)
+            }
+        
+        );
 
         // ----------------------------------- ORIGINAL ATTEMPT TO PORT BODY VISUALIZER -----------------------------------
 
@@ -154,11 +160,12 @@ export default {
         this.scene.add(this.cube)
 
         
-        this.camera.position.set(10,13,10)
+        this.camera.position.set(10,20,26)
         this.controls.update()
         this.controls.enableZoom = true
         
         
+
 
         
         
@@ -191,6 +198,39 @@ export default {
         morphGui.open();
 			
     },
+    addGUI: function() {
+        const gui = this.gui = new dat.GUI({autoPlace: false, width: 260, hideable: true});
+
+        const dispFolder = gui.addFolder('Display');
+        const envBackgroundCtrl = dispFolder.add(this.state, 'background');
+        envBackgroundCtrl.onChange(() => this.updateEnvironment());
+        const wireframeCtrl = dispFolder.add(this.state, 'wireframe');
+        wireframeCtrl.onChange(() => this.updateDisplay());
+        const skeletonCtrl = dispFolder.add(this.state, 'skeleton');
+        skeletonCtrl.onChange(() => this.updateDisplay());
+        const gridCtrl = dispFolder.add(this.state, 'grid');
+        gridCtrl.onChange(() => this.updateDisplay());
+        dispFolder.add(this.controls, 'autoRotate');
+        dispFolder.add(this.controls, 'screenSpacePanning');
+        // const bgColor1Ctrl = dispFolder.addColor(this.state, 'bgColor1');
+        // const bgColor2Ctrl = dispFolder.addColor(this.state, 'bgColor2');
+        // bgColor1Ctrl.onChange(() => this.updateBackground());
+        // bgColor2Ctrl.onChange(() => this.updateBackground());
+
+
+        // Morph target controls.
+        this.morphFolder = gui.addFolder('Morph Targets');
+        this.morphFolder.domElement.style.display = 'none';
+
+    },
+    updateDisplay: function() {
+        console.log("update display")
+    },
+    updateGui: function() {
+        this.morphCtrls.forEach((ctrl) => ctrl.remove());
+        this.morphCtrls.length = 0;
+        this.morphFolder.domElement.style.display = 'none';
+    },
     animate: function() {
         this.controls.update()
         requestAnimationFrame(this.animate)
@@ -213,6 +253,7 @@ export default {
             
     //     })
     this.init()
+    this.addGUI()
     this.animate()
   }
 }
